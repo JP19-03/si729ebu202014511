992e933c8517a8328eefdcaa2b24006fab980994
JP19-03 <u202014511@upc.edu.pe>
2024-11-24 11:25:36 -0500

    chore: project setup.

---
+++ src/main/java/org/hign/platform/u202014511/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -0,0 +1,32 @@
+package org.hign.platform.u202014511.shared.infrastructure.documentation.openapi.configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI platformOpenApi() {
+        // General configuration
+        var openApi = new OpenAPI();
+        openApi
+                .info(new Info()
+                        .title("HIGN. API")
+                        .description("API for HIGN. project")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0")
+                                .url("https://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("HIGN. Wiki Documentation")
+                        .url("(https://hign.org/"));
+
+
+        // Return OpenAPI configuration object with all the settings
+
+        return openApi;
+    }
+}
---
+++ src/test/java/org/hign/platform/u202014511/Si729ebu202014511ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hign.platform.u202014511;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Si729ebu202014511ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
